a528483b6dc1197fb7a604ea0f310eb55e383c22
shikha thakur <shikhawebstep@gmail.com>
2024-04-16 17:17:34 +0530

    add blog page or blogdata page and image

---
+++ src/GrabBite/Blog.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <>
+    <section>
+    <div className="container">
+     <div className="flex_box">
+     
+     </div>
+    </div>
+    </section>
+    </>
+  )
+}
+
+export default Blog
