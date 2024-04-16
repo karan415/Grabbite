af3f95f2f8cc842fdfa3d4883aeae4600e2e1dc4
shikha thakur <shikhawebstep@gmail.com>
2024-04-16 17:25:12 +0530

    make blog files

---
+++ src/GrabBite/Blog.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import BlogData from './BlogData'
+
+export const Blog = () => {
+  const [data,setData]=useState(BlogData)
+  return (
+    <>
+    
+    </>
+  )
+}
