0d42ca528578594bfe6597326a5ddebb26a0f06b
shikha thakur <shikhawebstep@gmail.com>
2024-04-16 18:45:37 +0530

    add content in blogsidebar

---
+++ src/GrabBite/BlogSidebar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const BlogSidebar = () => {
+  return (
+    <>
+    <div className="blogsidebar">
+       <div className="blogsidebar_inner">
+       <div className="searchblog">
+       <input type="search" name="" id="" placeholder='Enter' />
+       </div>
+        <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        </ul>
+
+       </div>
+    </div>
+    </>
+  )
+}
+
+export default BlogSidebar
