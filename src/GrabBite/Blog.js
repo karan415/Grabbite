c677d65023fa4426dcb90c8c585bc6c6a61bbb3c
shikha thakur <shikhawebstep@gmail.com>
2024-04-22 10:16:51 +0530

    add blogsidebare css

--- src/GrabBite/Blog.js
+++ src/GrabBite/Blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BlogData from './BlogData';
 import Footer from './Footer';
 import Header from './Header';
+import BlogSidebar from './BlogSidebar';
 
 const Blog = () => {
   return (
@@ -10,14 +11,15 @@ const Blog = () => {
       <section className='py-5'>
         <div className="container">
           <div className="row">
-            <div className="col-md-9">
+            <div className="col-md-9 ">
+            <div className="grid-container align-items-center overflow-hidden " >
               {BlogData.map((curElm, index) => {
                 return (
-                  <div className="row mt-4 align-items-center" key={index}>
-                    <div className="col-md-7">
+               
+                    <div className="grid-item p-3 border rounded-3 mt-4 " key={index}>
                       <img src={curElm.blogimg} alt="" />
-                    </div>
-                    <div className="col-md-5">
+                   
+                    <div className="mt-5">
                       <div className="contentblog">
                       <div className="d-flex justify-content-between mb-4">
                         <div className="span fw-bold">{curElm.date[0].month} /<span className='fw-bold'>{curElm.date[0].date} /</span>{curElm.date[0].year}</div>
@@ -29,12 +31,13 @@ const Blog = () => {
                         <button className="btn btn-primary">Read More</button>
                       </div>
                     </div>
-                  </div>
+                    </div>
                 );
               })}
+              </div>
             </div>
-            <div className="col-md-3">
-              {/* This is where you might add a sidebar or other content */}
+            <div className="col-md-3 mt-4  p-0">
+              <BlogSidebar/>
             </div>
           </div>
         </div>
--- src/GrabBite/BlogSidebar.js
+++ src/GrabBite/BlogSidebar.js
@@ -1,25 +1,23 @@
 import React from 'react'
-
+import img from '../Img/blogimage.jpg'
 const BlogSidebar = () => {
-  return (
-    <>
-    <div className="blogsidebar">
-       <div className="blogsidebar_inner">
-       <div className="searchblog">
-       <input type="search" name="" id="" placeholder='Enter' />
-       </div>
-        <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        </ul>
-
-       </div>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="blogsidebar bg-secondary p-4">
+                <div className="blogsidebar_inner">
+                    <div className="searchblog d-flex justify-content-between">
+                        <input type="search" name="" id="" className='py-2 w-100' placeholder='Search Here' /><input type="submit" className='py-2 bg-success border-0 text-white ' value="Submit" />
+                    </div>
+                    <ul>
+                        <li><img src={img} alt="" />Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, quis?</li>
+                        <li><img src={img} alt="" />A Practical Guide to Azure Security?</li>
+                        <li><img src={img} alt="" />How to Azure Automation Provisioning Work and Performance Fine-Tuning?</li>
+                        <li><img src={img} alt="" />Azure Application Deployment-Magento Best Guide</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default BlogSidebar
